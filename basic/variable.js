0785d3d43c38259d0d3b2974c25ac196bf383f44
guptakaustubhcreative <guptakaustubh96@gmail.com>
2025-12-26 14:13:23 +0000

    commit

---
+++ basic/variable.js
@@ -0,0 +1,5 @@
+const accountId="1234567890"
+let accountemail = "kaustubh@google.com"
+var accountpassword = "kaustubh1234"
+accountCity = "lucknow"
+console.table([accountId,accountemail,accountpassword,accountCity])
