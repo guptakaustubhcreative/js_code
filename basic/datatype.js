d77d91192d118ba19b5a4dbcbf86743c14bdbd00
guptakaustubhcreative <guptakaustubh96@gmail.com>
2025-12-26 14:48:35 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"//code all js code as new version
+console.log("hello world")
+let name ="kaustubh"
+let age= 20
+let isloggedin = true
+//primitive data types
+//number 2 to power 53
+//bigint
+//string
+//boolean true /false
+//null it is standalone value or use to assign value
+//undefined variable not assigned any value
+//symbol to create unique identifiers
+console.log(typeof name) 
+// use typeof to know datatype
+console.log(typeof null)
+// object (null is also object in js)
